f75ee22b34400cf185b546cb59f2aeab8beda281
AnkurRam2002 <ankurram2002@gmail.com>
2025-06-18 17:45:51 +0530

    first commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,8 +3,8 @@ import User from '../models/user.model.js';
 export const getUsers = async (req, res) => {
   try {
     const users = await User.find(
-      { _id: { $ne: req.userId } }, // exclude the current user
-      'name' // fetch only name and email fields
+      { _id: { $ne: req.userId } }, 
+      'name' 
     );
     res.json(users);
   } catch (error) {
